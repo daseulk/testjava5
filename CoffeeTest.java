783ce07d4bbd7159c2eb6296d4496ecda0bd0409
daseulkim <merilynkim37@gmail.com>
2020-08-19 15:07:57 +0900

    step

---
+++ CoffeeTest.java
@@ -0,0 +1,9 @@
+package com.tech3.inter_di1;
+
+public class CoffeeTest {
+
+	public static void main(String[] args) {
+		Coffee coffee =new Coffee();//참조변수 =커피 생성자 호출
+		coffee.coffeeType();
+	}
+}
